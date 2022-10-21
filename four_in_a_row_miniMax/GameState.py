90d480c5c8a8c1a54cd4ee6bdfc2b98d8235df5e
infinity-3 <carterscranton5@gmail.com>
2022-10-21 18:00:21 -0500

    find_in_a_rows() mostly writen. not tested x X x position not writen added variables for all 2/3 in a row varients added moves set list containing tuples of x and y

--- four_in_a_row_miniMax/GameState.py
+++ four_in_a_row_miniMax/GameState.py
@@ -7,6 +7,18 @@ class GameState(object):
         #GameState attributes
         self.state = []     #board state
         self.moves = {}    #set with all moves played for x
+
+        #number in-a-rows for each type
+        # pos 0 = x
+        # pos 1 = o     
+        self.twoOpen3 = [0,0]
+        self.oneOpen3 = [0,0]
+        self.twoOpen2 = [0,0]
+        self.oneOpen2 = [0,0]
+
+        self.eval = [0,0]
+
+
         
 
         #if optional parameter not given, initiaize to empty
@@ -26,7 +38,10 @@ class GameState(object):
     #returns the value in the given position
     def get(self, x, y):
         
-        return self.state[x][y]
+        if(x > 0 and x < 6 and y > 0 and y < 5):
+            return self.state[y][x]
+        else:
+            return 'w'          # W for wall e.g. edge of board/outside board. really only need to be differnt than x o and 0
 
     #sets a location on board to either an x or o
     #updates state as well as x and o move sets
@@ -46,17 +61,102 @@ class GameState(object):
 
 
 
-    #def detect_win(self, player):
-    #def detect_tie(self):
+    def detect_win(self, player):
+        pass
+    def detect_tie(self):
+        pass
+
+    #finds the number of 2 and 3 in a rows with correct openness, adds/subs gameState variables
+    #takes in the most recent move, should be run in set function
+    def find_in_a_rows(self, x, y):
+        baseMoveType = self.get(x,y)
 
 
-    #def calculate_eval(self, player):
+        for a in range(-1,2):                                               #offsets of -1 to 1 
+            for b in range(-1,2):                                           #check all adjacent squares 
+                if((x+a > 0 and x+a < 6) and (y+b > 0 and y+b < 5)):        #check if in board 
+                    searchType = self.get(x+a, y+b)
+
+                    if(searchType != 0):                            #check if not empty
+
+                        #if the adjacent square is the same as middle
+                        if(searchType == baseMoveType):
+                           
+                            #if middle value is between too of is own kind x X x ########################################################
+                            #needs to be implimented
+                            if(self.get(x-a, y-b) == searchType):
+                                pass
+
+
+                            #if two away is the same as base value (X x x)
+                            elif(self.get(x+(2*a), (y+(2*b))) == baseMoveType):
+                                if(self.get(x+(3*a), (y+(3*b))) == baseMoveType):
+                                    if(baseMoveType == 'x'):
+                                        self.eval[0] = 1000
+                                        self.eval[1] = -1000
+                                    else:
+                                        self.eval[1] = 1000
+                                        self.eval[0] = -1000
+                                    return
+
+
+                            #if two sides open (0 X x x 0)
+                            elif(self.get(x+(2*a), (y+(2*b))) == 0 and self.get(x-a, y-b) == 0):
+                                if(baseMoveType == 'x'): 
+                                    self.twoOpen2[0] += 1
+                                else:
+                                    self.twoOpen2[1] += 1
+
+                            #if one side open (X x x 0)
+                            elif(self.get(x+(2*a), (y+(2*b))) == 0 or self.get(x-a, y-b) == 0):
+                                if(baseMoveType == 'x'): 
+                                    self.twoOpen2[0] += 1
+                                else:
+                                    self.twoOpen2[1] += 1
+
+                        
+                        #if the adjacent square the oposite of middle ( X o)
+                        else:
+                            #if three in a row  of other type next to start position (X o o o)
+                            if(self.get(x+a, y+b) == SW(baseMoveType) and self.get(x+(2*a), (y+(2*b))) == SW(baseMoveType) and self.get(x+(3*a), (y+(3*b)) == SW(baseMoveType))):
+                                if(self.get(x+(4*a), (y+(4*b))) == 0):
+                                    if(baseMoveType == 'x'):
+                                        self.twoOpen3[1] += -1
+                                        self.oneOpen3[1] += 1
+                                    else:
+                                       self.twoOpen3[0] += -1
+                                       self.oneOpen3[0] += 1
+                                        
+                                else:
+                                    if(baseMoveType == 'x'):
+                                        self.oneOpen3[1] += -1
+                                    else:
+                                        self.oneOpen3[0] += -1
+
+                            #if two in a row of other type next to start position(X o o)
+                            elif((self.get(x+a, y+b) == SW(baseMoveType) and self.get(x+(2*a), (y+(2*b))) == SW(baseMoveType))):
+                                if(self.get(x+(3*a), (y+(3*b))) == 0):
+                                    if(baseMoveType == 'x'):
+                                        self.twoOpen2[1] += -1
+                                        self.oneOpen2[1] += 1
+                                    else:
+                                       self.twoOpen2[0] += -1
+                                       self.oneOpen2[0] += 1
+
+                                else:
+                                    if(baseMoveType == 'x'):
+                                        self.oneOpen2[1] += -1
+                                    else:
+                                        self.oneOpen2[0] += -1
+
+
+
+        
     #    #heuristic calculations
+    def calculate_eval(self):
+        pass
 
-    #def detect_3rows(self, player):
-    #def detect_2rows(self, player):
-
-
+ 
     
     def find_playable(self):
         avaliableMoves = [];
@@ -70,3 +170,11 @@ class GameState(object):
                                     avaliableMoves.append([x+i,y+j])
         return avaliableMoves
 
+
+#returns oposite of 
+def SW(type):
+    if(type == 'x'):
+        type = 'o'
+    elif(type == 'o'):
+        type = 'o'
+    return type
