6db41115a5375996b88aac4350b41f80a96fdf9e
cgs2xw <63666550+cgs2xw@users.noreply.github.com>
2022-10-21 15:14:47 -0500

    Added .moves  set to GameState + changes to initializations to acomidate it

--- four_in_a_row_miniMax/GameState.py
+++ four_in_a_row_miniMax/GameState.py
@@ -4,26 +4,42 @@
 class GameState(object):
     
     def __init__(self, state = None):
+        #GameState attributes
+        self.state = []     #board state
+        self.moves = {}    #set with all moves played for x
+        
 
-
-        self.state = []
-
+        #if optional parameter not given, initiaize to empty
         if (not(isinstance(state, GameState))):
             for i in range(5):
                 self.state.append([0] * 6)
+
+        #if Gamestate object is passed to constructor
         else:
             for i in range(5):
                 self.state.append(GameState[i])
+            self.moves = GameState.moves
+            
 
 
 
+    #returns the value in the given position
     def get(self, x, y):
         
         return self.state[x][y]
 
+    #sets a location on board to either an x or o
+    #updates state as well as x and o move sets
     def set(self, x, y, value):
-        self.state[x][y] = value
+        
+        if (value == 'x' or value == 'o'):
+            self.xmoves.add((x,y, value))
 
+        else:
+            raise Exception("incorrect input must be x or o")
+        self.state[y][x] = value
+
+    #prints board
     def print(self):
         for row in self.state:
             print(row)
@@ -40,6 +56,8 @@ class GameState(object):
     #def detect_3rows(self, player):
     #def detect_2rows(self, player):
 
+
+    
     def find_playable(self):
         avaliableMoves = [];
         for x in range(len(self.state)):
